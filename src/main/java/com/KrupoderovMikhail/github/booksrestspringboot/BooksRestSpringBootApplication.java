a5eeacaa1d1dcbc34c4b43fe0cb17d0f9a2b90c0
KrupoderovMikhail <mishka.crash@gmail.com>
2019-07-03 14:04:04 +0500

    Initial commit

---
+++ src/main/java/com/KrupoderovMikhail/github/booksrestspringboot/BooksRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.KrupoderovMikhail.github.booksrestspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksRestSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KrupoderovMikhail/github/booksrestspringboot/BooksRestSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.KrupoderovMikhail.github.booksrestspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BooksRestSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
